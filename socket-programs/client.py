5353e58e90ee36d0e3f0e07a9bbcceb816d566ab
Vaishakh Raveendran <vaishakhraveendran2002@gmail.com>
2024-01-09 19:10:03 +0530

    client sending message

--- socket-programs/client.py
+++ socket-programs/client.py
@@ -9,3 +9,14 @@ client = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 
 ADDR = (SERVER,PORT)
 client.connect(ADDR)
+
+def send(msg):
+    message=msg.encode(FORMAT)
+    msg_length=len(message)
+    send_length = str(msg_length).encode(FORMAT)
+    send_length += b' '*(HEADER-len(send_length))
+    client.send(send_length)
+    client.send(message)
+
+send("Hello World")
+send(DISCONNECT_MESSAGE)
