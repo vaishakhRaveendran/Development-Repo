a7b8d9c3c637da12416e495804770793139ddac8
Vaishakh Raveendran <vaishakhraveendran2002@gmail.com>
2024-01-12 10:47:52 +0530

    adding parameter to get requests

--- RestAPIs/rest.py
+++ RestAPIs/rest.py
@@ -5,13 +5,13 @@ app=Flask(__name__)
 api=Api(app)
 
 class helloworld(Resource):
-    def get(self,name):
-        return {'data':f"Hello {name}"}
+    def get(self,name,verb):
+        return {'name':f"Hello {name}",'verb':verb}
     def post(self):
         return {'data': "Posted"}
 
 
-api.add_resource(helloworld,"/helloworld/<string:name>")
+api.add_resource(helloworld,"/helloworld/<string:name>/<string:verb>")
 
 if __name__=='__main__':
  app.run(debug=True)
--- RestAPIs/test.py
+++ RestAPIs/test.py
@@ -1,4 +1,4 @@
 import requests
 BASE='http://127.0.0.1:5000/'
-response = requests.get(BASE+'helloworld/vaishakh')
+response = requests.get(BASE+'helloworld/vaishakh/fuck')
 print(response.json())
