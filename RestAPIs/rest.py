64232639ac9b258dd388c3bced191220cb9c1050
Vaishakh Raveendran <vaishakhraveendran2002@gmail.com>
2024-01-12 11:57:50 +0530

    implemented call for adding and viewing videos....

--- RestAPIs/rest.py
+++ RestAPIs/rest.py
@@ -1,5 +1,5 @@
 from flask import Flask,request
-from flask_restful import Api,Resource,reqparse
+from flask_restful import Api,Resource,reqparse,abort
 
 #Wrapping the api over the app
 app = Flask(__name__)
@@ -7,18 +7,28 @@ api = Api(app)
 
 #Video put argument parser
 video_put_args=reqparse.RequestParser()
-video_put_args.add_argument("name",type=str,help="Name of the video")
-video_put_args.add_argument("views",type=str,help="Views of the video")
-video_put_args.add_argument("likes",type=str,help="Likes of the video")
+video_put_args.add_argument("name",type=str,help="Name of the video",required=True)
+video_put_args.add_argument("views",type=int,help="Views of the video",required=True)
+video_put_args.add_argument("likes",type=int,help="Likes of the video",required=True)
 
+videos = {}
 class Video(Resource):
-    def get(self,video_id):
-        return videos[video_id]
-    def put(self,video_id):
-        args=video_put_args.parse_args()
-        return args
+    def get(self, video_id):
+        if video_id in videos:
+            return videos[video_id]
+        else:
+            abort(404,message="video id is not valid")
 
-api.add_resource(Video,"/video/<int:video_id>")
+    def post(self, video_id):
+        if video_id in videos:
+            return {"error": "Video already exists"}, 400
 
-if __name__=='__main__':
- app.run(debug=True)
+        args = video_put_args.parse_args()
+        videos[video_id] = args
+        return videos[video_id], 201
+
+
+api.add_resource(Video, "/video/<int:video_id>")
+
+if __name__ == '__main__':
+    app.run(debug=True)
