ea9aad78b1da51255c440f20fd02e59cc195ec58
Vaishakh Raveendran <vaishakhraveendran2002@gmail.com>
2024-01-12 12:21:20 +0530

    refactoring

--- RestAPIs/rest.py
+++ RestAPIs/rest.py
@@ -1,38 +1,45 @@
-from flask import Flask,request
-from flask_restful import Api,Resource,reqparse,abort
+from flask import Flask, request
+from flask_restful import Api, Resource, reqparse, abort
 
-#Wrapping the api over the app
+# Wrapping the API over the app
 app = Flask(__name__)
 api = Api(app)
 
-#Video put argument parser
-video_put_args=reqparse.RequestParser()
-video_put_args.add_argument("name",type=str,help="Name of the video",required=True)
-video_put_args.add_argument("views",type=int,help="Views of the video",required=True)
-video_put_args.add_argument("likes",type=int,help="Likes of the video",required=True)
+# Video put argument parser
+video_put_args = reqparse.RequestParser()
+video_put_args.add_argument("name", type=str, help="Name of the video", required=True)
+video_put_args.add_argument("views", type=int, help="Views of the video", required=True)
+video_put_args.add_argument("likes", type=int, help="Likes of the video", required=True)
 
 videos = {}
+
+
+def abort_if_video_id_not_exist(video_id):
+    if video_id not in videos:
+        abort(404, message="Video ID does not exist")
+
+
+def abort_if_video_id_exist(video_id):
+    if video_id in videos:
+        abort(404, message="Video ID already exists")
+
+
 class Video(Resource):
     def get(self, video_id):
-        if video_id in videos:
-            return videos[video_id]
-        else:
-            abort(404,message="video id is not valid")
+        abort_if_video_id_not_exist(video_id)
+        return videos[video_id]
 
     def post(self, video_id):
-        if video_id in videos:
-            return {"error": "Video already exists"}, 400
-
+        abort_if_video_id_exist(video_id)
         args = video_put_args.parse_args()
         videos[video_id] = args
         return videos[video_id], 201
 
-    def delete(self,video_id):
-        if video_id in videos:
-            del videos[video_id]
-            return '',204
-        else:
-            abort(404, message="video not found")
+    def delete(self, video_id):
+        abort_if_video_id_not_exist(video_id)
+        del videos[video_id]
+        return '', 204
+
 
 api.add_resource(Video, "/video/<int:video_id>")
 
