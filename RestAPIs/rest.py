11232cfadfbe586bea694984808ceff17560c6b7
Vaishakh Raveendran <vaishakhraveendran2002@gmail.com>
2024-01-12 11:24:07 +0530

    adding videos put argument  parser

--- RestAPIs/rest.py
+++ RestAPIs/rest.py
@@ -1,17 +1,24 @@
-from flask import Flask
-from flask_restful import Api,Resource
+from flask import Flask,request
+from flask_restful import Api,Resource,reqparse
 
-app=Flask(__name__)
-api=Api(app)
+#Wrapping the api over the app
+app = Flask(__name__)
+api = Api(app)
 
-class helloworld(Resource):
-    def get(self,name,verb):
-        return {'name':f"Hello {name}",'verb':verb}
-    def post(self):
-        return {'data': "Posted"}
+#Video put argument parser
+video_put_args=reqparse.RequestParser()
+video_put_args.add_argument("name",type=str,help="Name of the video")
+video_put_args.add_argument("views",type=str,help="Views of the video")
+video_put_args.add_argument("likes",type=str,help="Likes of the video")
 
+class Video(Resource):
+    def get(self,video_id):
+        return videos[video_id]
+    def put(self,video_id):
+        args=video_put_args.parse_args()
+        return args
 
-api.add_resource(helloworld,"/helloworld/<string:name>/<string:verb>")
+api.add_resource(Video,"/video/<int:video_id>")
 
 if __name__=='__main__':
  app.run(debug=True)
