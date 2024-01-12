aa2a7564c5448526adbea4e4c05d3e91bb131d00
Vaishakh Raveendran <vaishakhraveendran2002@gmail.com>
2024-01-12 12:47:51 +0530

    adding the video models

---
+++ RestAPIs/models.py
@@ -0,0 +1,9 @@
+from rest import db
+class VideoModel(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    views = db.Column(db.Integer, nullable=False)
+    likes = db.Column(db.Integer, nullable=False)
+
+    def __repr__(self):
+        return f'video:{self.name},views:{self.views},likes:{self.likes}'
