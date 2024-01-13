dd045ccba8beaa956a8e994d0dd544c58f037cd5
Vaishakh Raveendran <vaishakhraveendran2002@gmail.com>
2024-01-13 22:23:16 +0530

    createad webhook call back..

---
+++ Webhooks/main.py
@@ -0,0 +1,13 @@
+from flask import Flask,request
+
+app = Flask(__name__)
+
+#We want this function to be called when something happens.
+#We got a remote server which notify us about an event by calling this function without us asking..
+@app.route('/webhookcallback',method=['POST'])
+def hook():
+    print(request.data)
+    return "Hello world"
+
+if __name__ == "__main__" :
+    app.run(debug=True)
