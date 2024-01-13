a125afaeb294eb834025724e1c324574feafda01
Vaishakh Raveendran <vaishakhraveendran2002@gmail.com>
2024-01-13 22:24:00 +0530

    dd

--- Microservices/admin/admin/urls.py
+++ Microservices/admin/admin/urls.py
@@ -19,6 +19,6 @@ from django.urls import path,include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/",include('products.url'))
+    path("api/",include('products.urls'))
 
 ]
