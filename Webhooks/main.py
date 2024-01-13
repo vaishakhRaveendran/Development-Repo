08c1760a8c627a7a07ff0e3f67ecbd0b20bb02ff
Vaishakh Raveendran <vaishakhraveendran2002@gmail.com>
2024-01-13 22:38:55 +0530

    full webhook

--- Webhooks/main.py
+++ Webhooks/main.py
@@ -4,7 +4,8 @@ app = Flask(__name__)
 
 #We want this function to be called when something happens.
 #We got a remote server which notify us about an event by calling this function without us asking..
-@app.route('/webhookcallback',method=['POST'])
+
+@app.route('/webhookcallback',methods=['POST'])
 def hook():
     print(request.data)
     return "Hello world"
