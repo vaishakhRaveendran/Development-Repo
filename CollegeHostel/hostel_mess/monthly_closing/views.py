96c74bce34eb3dba94547f26da3635abccb20bf9
Vaishakh Raveendran <vaishakhraveendran2002@gmail.com>
2024-01-19 16:55:38 +0530

    views skelton created

--- CollegeHostel/hostel_mess/monthly_closing/views.py
+++ CollegeHostel/hostel_mess/monthly_closing/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def home(request):
+    return HttpResponse("Hello, this is your home page!")
+
+def create_closing(request):
+    return HttpResponse("Hello, this is your closing page!")
+
+def add_products(request):
+    return HttpResponse("Hello, this is your add product page!")
+
+def drop_products(request):
+    return HttpResponse("Hello, this is your drop product page!")
+
+def modify(request):
+    return HttpResponse("Hello, this is your modify page!")
