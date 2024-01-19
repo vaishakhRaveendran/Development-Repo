ba420e80d9ba6713f0a3b37cf21b11f40bf03476
Vaishakh Raveendran <vaishakhraveendran2002@gmail.com>
2024-01-19 16:55:17 +0530

    All routes created

---
+++ CollegeHostel/hostel_mess/monthly_closing/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+#Import views inside the urls.py of apps.
+from . import views
+urlpatterns = [
+    path('',views.home,name='monthly_closing-home'),
+    path('add_product/',views.add_products,name='monthly_closing-add_product'),
+    path('drop_product/',views.drop_products,name='monthly_closing-drop_product'),
+    path('create_closing/',views.create_closing,name='monthly_closing-create_closing'),
+    path('modify/',views.modify,name='monthly_closing-modify'),
+]
