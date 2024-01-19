35f7353653db0ecd4de7e86f26088cdf7c13730a
Vaishakh Raveendran <vaishakhraveendran2002@gmail.com>
2024-01-19 17:28:35 +0530

    adding templates

--- CollegeHostel/hostel_mess/hostel_mess/urls.py
+++ CollegeHostel/hostel_mess/hostel_mess/urls.py
@@ -20,5 +20,5 @@ from django.urls import path,include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path('monthly_closing/',include('monthly_closing.urls')),
+    path('',include('monthly_closing.urls')),
 ]
