33a32e0f3901076d2b9eac64be973181e6d3f053
Vaishakh Raveendran <vaishakhraveendran2002@gmail.com>
2024-01-19 21:58:18 +0530

    adding the product models

--- CollegeHostel/hostel_mess/monthly_closing/models.py
+++ CollegeHostel/hostel_mess/monthly_closing/models.py
@@ -1,3 +1,14 @@
+
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    productNo = models.IntegerField()
+    productName = models.CharField(max_length=255)
+    productUnit = models.CharField(max_length=50)
+    productPrice = models.DecimalField(max_digits=10, decimal_places=3)
+
+    def __str__(self):
+        return self.productName
+
+
+
