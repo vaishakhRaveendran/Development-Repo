531a580773a5c279978593563ea505a1ad6d9251
Vaishakh Raveendran <vaishakhraveendran2002@gmail.com>
2024-01-19 23:19:09 +0530

    table visualised in home page

--- CollegeHostel/hostel_mess/monthly_closing/models.py
+++ CollegeHostel/hostel_mess/monthly_closing/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Product(models.Model):
-    productNo = models.AutoField(primary_key=True, default=100)
+    productNo = models.AutoField(primary_key=True)
     productName = models.CharField(max_length=255)
     productUnit = models.CharField(max_length=50)
     productPrice = models.DecimalField(max_digits=10, decimal_places=3)
--- CollegeHostel/hostel_mess/monthly_closing/views.py
+++ CollegeHostel/hostel_mess/monthly_closing/views.py
@@ -1,18 +1,48 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .forms import ProductForm
+from .models import Product
 # Create your views here.
 def home(request):
-    return render(request,'monthly_closing/home.html')
-
-def create_closing(request):
-    return render(request,'monthly_closing/create_closing.html')
+    products = Product.objects.all()
+    print(products)
+    return render(request, 'monthly_closing/home.html', {'products': products})
 
 def add_products(request):
-    return render(request, 'monthly_closing/add_product.html')
+    if request.method == 'POST':
+        form = ProductForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('monthly_closing-home')
+    else:
+        form = ProductForm()
+    return render(request,'monthly_closing/add_product.html')
+
+def create_closing(request):
+    return render(request, 'monthly_closing/create_closing.html')
+
+
+
+
+
+
+
 
 def drop_products(request):
     return HttpResponse("Hello, this is your drop product page!")
 
 def modify(request):
     return HttpResponse("Hello, this is your modify page!")
+
+
+
+def create_product(request):
+    if request.method == 'POST':
+        form = ProductForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('product_list')  # Replace 'product_list' with your actual URL name
+    else:
+        form = ProductForm()
+
+    return render(request, 'create_product.html', {'form': form})
