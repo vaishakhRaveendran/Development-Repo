63120ce01b3d8a49b39dfe791456f94979d328c7
Vaishakh Raveendran <vaishakhraveendran2002@gmail.com>
2024-01-19 17:49:08 +0530

    added montly closing in installed apps

--- CollegeHostel/hostel_mess/hostel_mess/settings.py
+++ CollegeHostel/hostel_mess/hostel_mess/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     "django.contrib.admin",
     "django.contrib.auth",
+    "monthly_closing.apps.MonthlyClosingConfig",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
