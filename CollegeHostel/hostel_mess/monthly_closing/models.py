afea988163d4daaa6b495dfcb0c23977b9589934
Vaishakh Raveendran <vaishakhraveendran2002@gmail.com>
2024-01-19 22:30:28 +0530

    editing product using admin gui

--- CollegeHostel/hostel_mess/monthly_closing/models.py
+++ CollegeHostel/hostel_mess/monthly_closing/models.py
@@ -1,14 +1,10 @@
-
 from django.db import models
 
 class Product(models.Model):
-    productNo = models.IntegerField()
+    productNo = models.AutoField(primary_key=True, default=100)
     productName = models.CharField(max_length=255)
     productUnit = models.CharField(max_length=50)
     productPrice = models.DecimalField(max_digits=10, decimal_places=3)
 
     def __str__(self):
         return self.productName
-
-
-
