a8e0e2f4999dc378ff85ecd241fe4f47cfe6de4c
Vaishakh Raveendran <vaishakhraveendran2002@gmail.com>
2024-01-19 17:48:39 +0530

    successfully added templates and is working

--- CollegeHostel/hostel_mess/monthly_closing/apps.py
+++ CollegeHostel/hostel_mess/monthly_closing/apps.py
@@ -1,6 +1,5 @@
 from django.apps import AppConfig
 
-
 class MonthlyClosingConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "monthly_closing"
--- CollegeHostel/hostel_mess/monthly_closing/views.py
+++ CollegeHostel/hostel_mess/monthly_closing/views.py
@@ -3,13 +3,13 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse("Hello, this is your home page!")
+    return render(request,'monthly_closing/home.html')
 
 def create_closing(request):
-    return HttpResponse("Hello, this is your closing page!")
+    return render(request,'monthly_closing/create_closing.html')
 
 def add_products(request):
-    return HttpResponse("Hello, this is your add product page!")
+    return render(request, 'monthly_closing/add_product.html')
 
 def drop_products(request):
     return HttpResponse("Hello, this is your drop product page!")
