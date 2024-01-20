30b2040d6ce19b0e8772eda7bfe719f381f24561
Vaishakh Raveendran <vaishakhraveendran2002@gmail.com>
2024-01-20 19:06:23 +0530

    adding new endpoint

--- CollegeHostel/hostel_mess/monthly_closing/urls.py
+++ CollegeHostel/hostel_mess/monthly_closing/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('drop_product/',views.drop_products,name='monthly_closing-drop_product'),
     path('create_closing/',views.create_closing,name='monthly_closing-create_closing'),
     path('modify/',views.modify,name='monthly_closing-modify'),
+    path('get_product_details/<int:product_id>/',views.get_product_details, name='get_product_details')
 ]
