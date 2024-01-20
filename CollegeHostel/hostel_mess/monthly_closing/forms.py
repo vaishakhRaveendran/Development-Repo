d913095a3b87f07b2e5356f255f3abef48277bed
Vaishakh Raveendran <vaishakhraveendran2002@gmail.com>
2024-01-20 19:05:58 +0530

    adding monthly closings form

--- CollegeHostel/hostel_mess/monthly_closing/forms.py
+++ CollegeHostel/hostel_mess/monthly_closing/forms.py
@@ -10,3 +10,29 @@ class addForm(forms.ModelForm):
 class dropForm(forms.Form):
     productId = forms.ChoiceField(widget=forms.Select(attrs={'class': 'form-control'}))
     productId.choices = Product.objects.values_list('productNo', 'productName')
+
+
+
+##################################################################################################################
+class productForm(forms.Form):
+    productName = forms.ChoiceField(widget=forms.Select(attrs={'class': 'form-control'}))
+    quantity = forms.DecimalField(label='Quantity/Weight')
+    unit = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control', 'readonly': True}))
+    unit_price = forms.DecimalField(widget=forms.NumberInput(attrs={'class': 'form-control', 'readonly': True}))
+    total_price = forms.DecimalField(widget=forms.NumberInput(attrs={'class': 'form-control', 'readonly': True}), required=False)
+
+
+    def __init__(self, *args, **kwargs):
+        super(productForm, self).__init__(*args, **kwargs)
+        # Populate the choices for productName field
+        self.fields['productName'].choices = Product.objects.values_list('productNo', 'productName')
+
+    def clean(self):
+        cleaned_data = super().clean()
+        quantity = cleaned_data.get('quantity')
+        unit_price = cleaned_data.get('unit_price')
+
+        if quantity and unit_price:
+            cleaned_data['total_price'] = quantity * unit_price
+
+        return cleaned_data
