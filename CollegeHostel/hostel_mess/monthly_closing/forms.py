4d216688cf39aff169119521e3070df6dbeca3b1
Vaishakh Raveendran <vaishakhraveendran2002@gmail.com>
2024-01-20 15:29:43 +0530

    added drop product form and choice field

--- CollegeHostel/hostel_mess/monthly_closing/forms.py
+++ CollegeHostel/hostel_mess/monthly_closing/forms.py
@@ -1,8 +1,12 @@
 from django import forms
 from .models import Product
 
-class ProductForm(forms.ModelForm):
+class addForm(forms.ModelForm):
     class Meta:
         model = Product
         fields = ['productName', 'productUnit', 'productPrice']
     productUnit = forms.ChoiceField(choices=Product.UNIT_CHOICES, widget=forms.Select(attrs={'class': 'form-control'}))
+
+class dropForm(forms.Form):
+    productId = forms.ChoiceField(widget=forms.Select(attrs={'class': 'form-control'}))
+    productId.choices = Product.objects.values_list('productNo', 'productName')
