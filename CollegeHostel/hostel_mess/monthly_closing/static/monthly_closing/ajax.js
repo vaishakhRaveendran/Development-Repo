3f700113fb633e34d4ebbb133c2a5cf1f2c41530
Vaishakh Raveendran <vaishakhraveendran2002@gmail.com>
2024-01-23 02:54:48 +0530

    patch

--- CollegeHostel/hostel_mess/monthly_closing/static/monthly_closing/ajax.js
+++ CollegeHostel/hostel_mess/monthly_closing/static/monthly_closing/ajax.js
@@ -1,13 +1,14 @@
 // ajax.js
 function fetchProductDetails(productId) {
-    console.log("fetch working!")
+    console.log("fetch working!");
     $.ajax({
         url: '/get_product_details/' + productId + '/',
         type: 'GET',
         success: function(data) {
-            if (data && 'unit' in data && 'unit_price' in data) {
-                $('[id^=id_unit]').val(data.unit);
-                $('[id^=id_unit_price]').val(data.unit_price);
+            if (data && 'unit' in data && 'price' in data) {
+                console.log(data);
+                $('[id^=id_unit]').val(data['unit']);
+                $('[id^=id_unit_price]').val(parseInt(data['price'], 10));
                 updateTotalPrice();
             } else {
                 console.error('Invalid data format received from server.');
