812c6bed9df2e5bb417fa724eeb7bd4261792bbe
Vaishakh Raveendran <vaishakhraveendran2002@gmail.com>
2024-01-26 13:30:58 +0530

    Setting up and basic routing.....

--- Express/4-natours/starter/app.js
+++ Express/4-natours/starter/app.js
@@ -1,6 +1,18 @@
 const express=require('express');
 const app=express();
+
+
+app.get('/',(req, res)=>{
+ res.status(200).json({message:'Hello from the server side!',app:'Natours'});
+
+});
+
+app.post('/',(req,res)=>{
+ res.status(200).send('We love you');
+})
+
+
 const PORT=3000;
 app.listen(PORT,()=>{
- console.log(`listening on port ${PORT}`);
+ console.log(`App listening on port ${PORT}...`);
 })
