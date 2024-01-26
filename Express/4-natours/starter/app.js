7a3252dee53684155f8064f0f857acfe906cdb00
Vaishakh Raveendran <vaishakhraveendran2002@gmail.com>
2024-01-26 22:48:12 +0530

    responding to parameters in url

--- Express/4-natours/starter/app.js
+++ Express/4-natours/starter/app.js
@@ -3,20 +3,40 @@ const fs =require('fs');
 const app=express();
 app.use(express.json());
 
-
+/////////////////////////////////////////////////////////////////////////////////////////////////
 //Endpoint Tours
 const tours=JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`));
 //tours is a json object
 //get tours info
-app.get('/api/v1/tours',(req, res)=>{
-   res.status(200).json({
+app.get('/api/v1/tours/:id?',(req, res)=>{
+   const  id = req.params.id*1;
+
+   if (id > tours.length){
+        return res.status(404).json({
+            status: 'fail',
+            message: 'Invalid ID',
+        });
+    }
+    
+    const tour=tours.find(el=>el.id===id);
+    if (!tour){
+        return res.status(404).json({
+            status: 'fail',
+            message: 'Invalid ID',
+        });
+    }
+
+    res.status(200).json({
      status: 'success',
      result:tours.length,
       data:{
-        tours:tours
+        tours:tour
        }
     });
+
 });
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //add new tours
 app.post('/api/v1/tours', (req, res) => {
     const newId = tours[tours.length - 1].id + 1;
@@ -39,9 +59,6 @@ app.post('/api/v1/tours', (req, res) => {
         }
     });
 });
-
-
-
 
 //Create listner.
 const PORT=3000;
