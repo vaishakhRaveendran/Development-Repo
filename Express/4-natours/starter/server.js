1ac0667f588e732266d1e8e661d86645c8c46d4c
Vaishakh Raveendran <vaishakhraveendran2002@gmail.com>
2024-02-03 22:30:33 +0530

    mangoose connections

--- Express/4-natours/starter/server.js
+++ Express/4-natours/starter/server.js
@@ -1,8 +1,21 @@
 const dotenv = require('dotenv');
+const mongoose=require('mongoose');
 dotenv.config({path:'./config.env'});
 app = require('./app');
 console.log(process.env);
+const DB=process.env.DATABASE.replace(
+    '<password>',
+    process.env.PASSWORD
+);
+mongoose.connect(DB,{
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: true
+}).then(conn => {
+    console.log(conn.connection);
+    console.log('Connection Successful');
+});
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
+app.listen(port,() => {
     console.log(`App listening on port ${port}...`);
 });
